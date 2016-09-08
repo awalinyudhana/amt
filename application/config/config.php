ab32d6b4d7807605d1ac197a1b3e5573a6cf0e04
awalin yudhana <otoy.destroyed@gmail.com>
2016-09-08 14:42:14 +0700

    init

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1:8001';
+$config['base_url'] = 'http://amt-awalinyudhana.rhcloud.com/';
 
 /*
 |--------------------------------------------------------------------------
